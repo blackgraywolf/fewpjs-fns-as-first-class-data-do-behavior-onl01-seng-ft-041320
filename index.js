599c49a39efe0e710f372d4d6711cf3f1b7fef9b
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-07-16 23:28:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,9 +11,15 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 
+<<<<<<< HEAD
 function greet(time) {
   //The parseInt() function parses a string and returns an integer. 
    let hour = parseInt(time, 10);
+=======
+function greet(hour) {
+  //let hour = Date.getHours() 
+   const hour = parseInt(timeStr, 10);
+>>>>>>> b138f3ce25497d5f8f7ffc7a14233a769b30341e
   if ( hour < 12) 
   {
     return "Good Morning";
@@ -21,11 +27,17 @@ function greet(time) {
   }
   else if ( hour > 17) {
     
+<<<<<<< HEAD
     return "Good Evening"
 }
 else {
   return "Good Afternoon"
 }
+=======
+    return "Good Evening"}
+    else {
+  return "Good Afternoon"}
+>>>>>>> b138f3ce25497d5f8f7ffc7a14233a769b30341e
 }
 /* Write your implementation of displayMessage() */
 
